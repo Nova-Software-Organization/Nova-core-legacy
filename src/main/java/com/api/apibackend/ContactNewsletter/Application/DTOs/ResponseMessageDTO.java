699aab4e0d90225ede05320a37f1c642ecfa2353
kaue de matos <kaue178hotimail@gmail.com>
2023-12-31 14:35:26 -0300

    adicionando documentação no reponse do contact newslleter

--- src/main/java/com/api/apibackend/ContactNewsletter/Application/DTOs/ResponseMessageDTO.java
+++ src/main/java/com/api/apibackend/ContactNewsletter/Application/DTOs/ResponseMessageDTO.java
@@ -1,5 +1,13 @@
 package com.api.apibackend.ContactNewsletter.Application.DTOs;
 
+/**
+ * ----------------------------------------------------------------------------
+ * Autor: Kaue de Matos
+ * Empresa: Nova Software
+ * Propriedade da Empresa: Todos os direitos reservados
+ * ----------------------------------------------------------------------------
+ */
+
 import lombok.Data;
 
 @Data
